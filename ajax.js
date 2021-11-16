5dfd9bb1b07b36a8d4f24b1bb33e7a41593524b2
Abhishek-KK <93716061+Abhishek-KK@users.noreply.github.com>
2021-11-16 18:38:42 +0530

    Changes

--- ajax.js
+++ ajax.js
@@ -1,24 +1,23 @@
 
-//Creating a function which will be called when button is clicked
+
 function ajax(){
 
-// Creating an XHR object and assign it to a var xhttp
+
 var xhttp = new XMLHttpRequest();
 
-//Event listener to check if the status of req is changing
+
 xhttp.onreadystatechange = function(){
 
-    //if readystatus is changed to done(4) & status changed to 200(server processed the req)
+ 
     if(this.readyState==4 && this.status==200){
 
-        // Copying contents from server to div block, reponseText contains content of server
-        // JSON.parse will convert the JSON text to normal text inside reponse Text
+       
         var response = JSON.parse(this.responseText);       
     	var tbody = document.getElementById('tbody');
         var Jgrocery = response.grocery;
         
 		for (var i = 0; i < Jgrocery.length; i++){
-			var row = `<tr class = "table-danger">
+			var row = `<tr class = "text-dark">
 
 							<td>${Jgrocery[i].SerialNo}</td>
 							<td>${Jgrocery[i].Name}</td>
